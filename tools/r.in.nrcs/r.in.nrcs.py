b65bf204e7932318a203a7a22b6677f177ec2370
Corey White <ctwhite48@gmail.com>
2025-01-15 12:24:22 -0500

    Updated analysis

---
+++ tools/r.in.nrcs/r.in.nrcs.py
@@ -0,0 +1,37 @@
+# https://ncss-tech.github.io/soilDB/
+# https://ncss-tech.github.io/aqp/
+
+# https://ncss-tech.github.io/AQP/aqp/water-retention-curves.html
+
+# SSURGO Documentation:
+
+#     https://www.nrcs.usda.gov/resources/data-and-reports/ssurgo/stats2go-metadata
+
+#     The data model diagram and table/columns description reports
+#     are probably the most important documents there. This is
+#     large and complicated database; I'm happy
+#     to help with questions any time.
+
+#     Soil Data Access: https://sdmdataaccess.nrcs.usda.gov/
+
+#     Python interface to SDA: https://github.com/ncss-tech/pysda
+
+#     SDA tutorial, focusing on the R interface:
+#       https://ncss-tech.github.io/AQP/soilDB/SDA-tutorial.html
+
+
+# SoilWeb:
+
+#     WWW interface: https://casoilresource.lawr.ucdavis.edu/gmap/
+
+#     Soil Data Explorer: https://casoilresource.lawr.ucdavis.edu/sde/
+
+
+# Related R Packages:
+
+#     https://ncss-tech.github.io/aqp/articles/aqp-overview.html
+
+#     https://ncss-tech.github.io/soilDB/
+
+#     gridded map unit key WCS:
+#       https://ncss-tech.github.io/AQP/soilDB/WCS-demonstration-01.html
