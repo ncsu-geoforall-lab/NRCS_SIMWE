079cc28de2155d1e108d81f636c65bbffe919781
Corey White <smortopahri@gmail.com>
2026-02-04 21:11:01 -0500

    Added resolution and NAIP year (#3)

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -23,7 +23,10 @@ def main():
                 import_elevation(project_name, resolution)
                 # Import the SSURGO MUKEY raster
                 import_ssurgo_mukey(project_name, resolution)
-                download_naip(project_name)
+
+                # Download NAIP data
+                # download_naip(naip)
+                # patch_and_composite_naip(naip)
 
             except ValueError:
                 exit(1)
@@ -96,19 +99,21 @@ def download_naip(year=2022):
     print("Downloading NAIP data")
     # Create a new mapset to store raw naip data
     gs.run_command("g.mapset", mapset="naip", flags="c")
+    gs.run_command("g.region", raster="elevation", res=1, flags="a")
     # Import the raster into GRASS GIS
     gs.run_command(
-        "t.stac.import",
+        "t.stac.item",
         overwrite=True,
         url="https://planetarycomputer.microsoft.com/api/stac/v1",
         request_method="POST",
-        collections="naip",
+        collection_id="naip",
         datetime=year,
         asset_keys="image",
         resolution="value",
         resolution_value=1,
         extent="region",
         nprocs=10,
+        flags="d",
         memory=10000,
     )
 
@@ -122,21 +127,22 @@ def patch_and_composite_naip(year=2022):
         image_list = gs.read_command(
             "g.list", type="raster", pattern=f"*.{i}", separator="comma"
         ).strip()
-
+        patch_name = f"naip_{year}_{band_name}"
+        print(f"Creating Patch: {patch_name}")
         gs.run_command(
             "r.patch",
             input=image_list,
-            output=f"naip_{year}.{band_name}",
-            nprocs=4,
+            output=patch_name,
+            nprocs=1,
             memory=2100,
             overwrite=True,
         )
 
     gs.run_command(
         "r.composite",
-        red=f"naip_{year}.red",
-        green=f"naip_{year}.green",
-        blue=f"naip_{year}.blue",
+        red=f"naip_{year}_red",
+        green=f"naip_{year}_green",
+        blue=f"naip_{year}_blue",
         output=f"naip_{year}_rgb",
         overwrite=True,
     )
@@ -152,6 +158,12 @@ if __name__ == "__main__":
     )
 
     import grass.script as gs
+
+    # We need to import gutiles after grass is part of the path
+    from gutiles import install_addons
+
+    # Install rquired GRASS addons
+    install_addons(addons_file="addons.txt")
 
     # Execute the main function
     sys.exit(main())
